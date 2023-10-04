cd1c2cd8df27b717567aab14f5c55e0295d05580
Jiwoong Moon <woongsnote@gmail.com>
2023-10-04 13:44:28 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex h-screen flex-col items-center mx-auto text-white">
         <ProfileCard username={USER_NAME} />
       </main>
-      <footer className="fixed bottom-0 w-full text-center pb-4 text-white px-2">
+      <footer className="bottom-0 w-full text-center pb-4 text-white px-2">
         <p> © 2023. 문지웅 | All rights reserved.</p>
       </footer>
     </>
